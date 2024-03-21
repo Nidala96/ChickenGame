eb8ebb4bd1697f0a6b07453b7ee4223209659ff2
Dagcreate <71767992+Dagcreate@users.noreply.github.com>
2024-03-21 15:01:41 +0100

    Base Chicken Added
    -Added base_chicken node
    -Added animation for idle and run for the base chicken enemy

--- Scenes/shotgun.gd
+++ Scenes/shotgun.gd
@@ -6,23 +6,22 @@ var crosshair_texture = preload("res://Assets/crosshair.png")
 var node2d
 
 func _ready():
+	#Set crosshair on mouse position
 	Input.set_custom_mouse_cursor(crosshair_texture)
 	node2d = %BulletSpawnPoint
-	
+
 func _physics_process(delta):
-		var mouse_position = get_global_mouse_position()
-		look_at(mouse_position)
-		
+	var mouse_position = get_global_mouse_position()
+	look_at(mouse_position)
+
 func _input(event):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
-		
+
 func shoot():
-	
 	var bullet = bullet_scene.instantiate() as Bullet
 	get_tree().root.add_child(bullet)
 	var move_direction = (get_global_mouse_position() - global_position).normalized()
 	bullet.move_direction = move_direction
 	bullet.global_position = node2d.global_position
 	bullet.rotation = move_direction.angle()
-	
