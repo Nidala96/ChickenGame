e7dd47182bf070772e7900fe22b57dea8626cad8
Dagcreate <71767992+Dagcreate@users.noreply.github.com>
2024-04-05 14:13:27 +0200

    Audio Progress

--- Scenes/background_chicken.gd
+++ Scenes/background_chicken.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 func _physics_process(delta):
 	animation()
+	delete()
 
 func animation():
 	%AnimatedSprite2D.play("idle")
+
+func delete():
+	if Global.chickenEvent:
+		queue_free()
